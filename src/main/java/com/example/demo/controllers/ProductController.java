7a6c2823aa21218162f8bc12c5ebaca53a8550f8
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-08 19:37:49 -0300

    feat: findById adicionado

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -9,19 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.rmi.server.UID;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
-
 
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
@@ -40,4 +39,17 @@ public class ProductController {
     public ResponseEntity<List<ProductModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
+
+
+    @GetMapping("/products/{id}")
+    public  ResponseEntity<Object> getById(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productModel = productRepository.findById(id);
+
+        if(productModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found ");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(productModel.get());
+    }
+
 }
